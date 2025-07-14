98810888d33f029e09e7f73757d88189530e2161
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-07-14 19:47:11 +0500

    fix user id

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -30,12 +30,12 @@ func NewJWTManager(jwtSecret string, tokenExpires int) *JWTManager {
 // 	return token.SignedString(jwtKey)
 // }
 
-func (a *JWTManager) GenerateJWT(userId int, userLogin string) (string, int, error) {
+func (a *JWTManager) GenerateJWT(userID int, userLogin string) (string, int, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, model.Claims{
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(a.tokenExpires) * time.Hour)), // Токен живет 24 часа
 		},
-		UserID:    userId,
+		UserID:    userID,
 		UserLogin: userLogin,
 	})
 	tokenString, err := token.SignedString([]byte(a.jwtSecret))
