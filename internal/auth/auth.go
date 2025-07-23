855f2e44a6b4d2ed9ef567d63985f73cfc1e7522
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-07-23 16:24:05 +0500

    fix

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,20 +16,6 @@ func NewJWTManager(jwtSecret string, tokenExpires int) *JWTManager {
 	return &JWTManager{jwtSecret: jwtSecret, tokenExpires: tokenExpires}
 }
 
-// const jwtKey = []byte("your-very-secret-key") // Заменить на реальный секрет из конфига
-
-// func GenerateJWT(login string) (string, error) {
-// 	claims := &jwt.RegisteredClaims{
-// 		Subject:   login,
-// 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), // Токен живет 24 часа
-// 		IssuedAt:  jwt.NewNumericDate(time.Now()),
-// 		Issuer:    "your-app-name",
-// 	}
-
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-// 	return token.SignedString(jwtKey)
-// }
-
 func (a *JWTManager) GenerateJWT(userID int, userLogin string) (string, int, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, model.Claims{
 		RegisteredClaims: jwt.RegisteredClaims{
